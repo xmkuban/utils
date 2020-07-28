d478e3325708a83d47d5d1e4d6dc7e5840b9e88b
dcsunny <934831065@qq.com>
2020-07-28 22:35:39 +0800

    完善锁机制

--- utils/lock.go
+++ utils/lock.go
@@ -11,6 +11,9 @@ import (
 var (
 	registerServiceLock = sync.Mutex{}
 	lockCache           cache.Cache
+
+	serverLockTimeout = time.Minute      //server锁过期时间
+	keyLockTimeout    = time.Second * 30 //server下的key锁过期时间
 )
 
 func init() {
@@ -18,21 +21,47 @@ func init() {
 }
 
 func GetServiceLock(service string, key interface{}) *sync.Mutex {
-	k := service + fmt.Sprint(key)
-	lock := lockCache.Get(k)
-	if lock == nil {
+	lockServer := lockCache.Get(service)
+	if lockServer == nil {
 		registerServiceLock.Lock()
-		lock = lockCache.Get(k)
-		if lock == nil {
-			lock = new(sync.Mutex)
-			lockCache.Put(k, lock, time.Second*30)
-			registerServiceLock.Unlock()
-			return lock.(*sync.Mutex)
+		lockServer = lockCache.Get(service)
+		if lockServer == nil {
+			lockServer = new(sync.Mutex)
+			lockCache.Put(service, lockServer, serverLockTimeout)
 		}
 		registerServiceLock.Unlock()
 	}
-	registerServiceLock.Lock()
-	lockCache.Put(k, lock, time.Second*30)
-	registerServiceLock.Unlock()
+	_lockServer := lockServer.(*sync.Mutex)
+	k := service + fmt.Sprint(key)
+	lock := lockCache.Get(k)
+	if lock == nil {
+		_lockServer.Lock()
+		lock = lockCache.Get(k)
+		if lock == nil {
+			lock = new(sync.Mutex)
+			lockCache.Put(k, lock, keyLockTimeout)
+			_lockServer.Unlock()
+			return lock.(*sync.Mutex)
+		}
+		_lockServer.Unlock()
+	} else {
+		_lockServer.Lock()
+		lockCache.Put(k, lock, keyLockTimeout)
+		_lockServer.Unlock()
+	}
 	return lock.(*sync.Mutex)
+}
+
+func SetServerLockTimeout(t time.Duration) {
+	if t.Nanoseconds() == 0 {
+		return
+	}
+	serverLockTimeout = t
+}
+
+func SetKeyLockTimeout(t time.Duration) {
+	if t.Nanoseconds() == 0 {
+		return
+	}
+	keyLockTimeout = t
 }
