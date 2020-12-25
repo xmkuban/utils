100115d1fdf3e25c6bd1a72b7b0828acf7682c89
dcsunny <934831065@qq.com>
2020-12-25 16:38:48 +0800

    Zrangebyscore 调整一下参数顺序

--- db/redis.go
+++ db/redis.go
@@ -468,11 +468,11 @@ func (r *RedisWrap) Zrevrangebyscore(key string, max int64, min int64, withscore
 	return redis.Strings(conn.Do("ZREVRANGEBYSCORE", key, max, min, "WITHSCORES"))
 }
 
-func (r *RedisWrap) Zrangebyscore(key string, max int64, min int64, withscore bool) ([]string, error) {
+func (r *RedisWrap) Zrangebyscore(key string, min int64, max int64, withscore bool) ([]string, error) {
 	conn := r.GetConn()
 	defer conn.Close()
 	if !withscore {
-		return redis.Strings(conn.Do("ZRANGEBYSCORE", key, max, min))
+		return redis.Strings(conn.Do("ZRANGEBYSCORE", key, min, max))
 	}
-	return redis.Strings(conn.Do("ZRANGEBYSCORE", key, max, min, "WITHSCORES"))
+	return redis.Strings(conn.Do("ZRANGEBYSCORE", key, min, max, "WITHSCORES"))
 }
